76b1e0550d3d7028b7d4d217e15296088dba1c95
pop it <230358@astanit.edu.kz>
2024-02-27 23:45:45 +0600

    borrowBook

--- src/interfaces/ordmember/borrowBook.java
+++ src/interfaces/ordmember/borrowBook.java
@@ -1,4 +1,9 @@
 package interfaces.ordmember;
 
+import java.sql.SQLException;
+
 public interface borrowBook {
+    public void borrowBook();
+
+    public void borrowBook(int bookId) throws SQLException;
 }
