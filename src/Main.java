563690568dbbcdae22117c4e6d50779bc679366a
b_baccala <231388@astanait.edu.kz>
2024-02-27 20:03:39 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    //Create a scanner
+    private static final Scanner scanner = new Scanner(System.in);
+    //Connection string
+    public static String connString = "jdbc:postgresql://localhost:5432/library11";
+    public static void main(String[] args) {
+        //Greeting a User
+        System.out.println("Hello!");
+    }
+}
