95f4ba7e5564a6bd36a7bd045c1b889077c2da54
b_baccala <231388@astanait.edu.kz>
2024-02-27 20:09:46 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.Connection;
 import java.util.Scanner;
 
 public class Main {
@@ -9,5 +10,15 @@ public class Main {
     public static void main(String[] args) {
         //Greeting a User
         System.out.println("Hello!");
+        //Declare a connection variable
+        Connection conn = null;
+        //logOrSignUp variable declaration
+        String logOrSignUp;
+        //Asking for Log In or Sign Up
+        do {
+            System.out.println("Type 1 for Login ot type 2 for Sign Up: ");
+            logOrSignUp = scanner.nextLine();
+        } while (logOrSignUp.isEmpty() && !(logOrSignUp.equals("2") || logOrSignUp.equals("1")));
+
     }
 }
