889373143e987a3d30d7ce8233dccdcbdbd65042
pop it <230358@astanit.edu.kz>
2024-02-27 23:46:28 +0600

    interface changed - returnBook

--- src/interfaces/ordmember/returnBook.java
+++ src/interfaces/ordmember/returnBook.java
@@ -1,4 +1,9 @@
 package interfaces.ordmember;
 
+import java.sql.SQLException;
+
 public interface returnBook {
+    public void returnBook();
+
+    public void returnBook(int bookId) throws SQLException;
 }
